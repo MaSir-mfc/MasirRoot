bee9ca65ca404ea63ca7958487cb2f3a7e88df70
mfc <914246767@qq.com>
2016-06-01 14:14:23 +0800

    继续查找index问题

---
+++ Masir/Web/Htmx/RedirectPage.cs
@@ -0,0 +1,26 @@
+﻿using Masir.Web.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masir.Web.Htmx
+{
+    /// <summary>
+    /// 跳转页面
+    /// </summary>
+    public class RedirectPage : HtmxHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void LoadMainTemplate()
+        {
+            string url = System.Text.RegularExpressions.Regex.Replace(MaUrl.Current.AbsolutePath, m_maPage.MappedRegexStr, m_maPage.TemplateRegexStr);
+            url = url.Replace("{#QueryString#}", Request.QueryString.ToString());
+            Response.Redirect(url);
+            Response.End();
+        }
+    }
+}
